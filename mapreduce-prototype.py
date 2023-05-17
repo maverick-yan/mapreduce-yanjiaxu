f060aec67dc1729ed950509ae2c0fc804e48029f
Citrus Yan <35949676+maverick-yan@users.noreply.github.com>
2023-05-17 15:19:29 +0800

    finish framework for function run(), run_reducer() & run_mapper

--- mapreduce-prototype.py
+++ mapreduce-prototype.py
@@ -34,3 +34,66 @@ class MapReduce(object):
         """
         pass
     
+    def run_mapper(self, index):
+        """_summary_
+
+        Args:
+            index (int): the index of the thread    
+        """
+        # read a key 
+        # read a value 
+        # get the result of the mapper
+        # store the result to reducer
+        pass
+    
+    def run_reducer(self, index):
+        """reducer
+
+        Args:
+            index (int): index of the reducer thread
+        """
+        # load results from mapper
+        # for each key, do reduce
+        # store results
+        
+        pass
+    
+    def run(self, join=False):
+        """
+        where map and reduce operations take place
+        
+        
+        """
+        
+        # initialize mappers list
+        map_workers = []
+        
+        # initialize reducers list
+        reduce_workers = []
+        
+        #map
+        for thread_id in range(self.n_mappers):
+            p = Process(target=self.run_mapper, args=(thread_id,))
+            p.start()
+            map_workers.append(p)
+        [t.join() for t in map_workers]
+        
+        
+        
+        #reduce
+        
+        for thread_id in range(self.n_reducers):
+            p = Process(target=self.run_reducer, args=(thread_id,))
+            p.start()
+            reduce_workers.append(p)
+        [t.join() for t in reduce_workers]
+        
+        if join:
+            self.join_outputs()
+        
+
+        
+            
+
+
+        
